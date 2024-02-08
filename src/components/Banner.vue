8ea5c1119f7cb54d72c1729f29c590d04c6762cc
Jonathan Nobrega <eng.jonathan.moraes@gmail.com>
2024-02-08 14:30:23 -0500

    first commit

---
+++ src/components/Banner.vue
@@ -0,0 +1,73 @@
+<template>
+    <header class="banner">
+        <div class="apresentacao">
+            <img src="../assets/imagens/logo.svg" alt="Logo da Cooking up" class="logo">
+
+            <p class="cabecalho-lg frase-cabecalho">
+                <span class="texto-verde">
+                    A feast of ideas to
+                </span>
+                awaken the cook inside of you
+            </p>
+            <p class="subtitulo-lg">
+                Explore new recepies every day with the available ingredients within your reach!
+            </p>
+        </div>
+
+        <img src="../assets/imagens/foto-banner.png" alt="Picture of a woman cooking">
+    </header>
+</template>
+
+<style scoped>
+.banner {
+    padding: 4rem 7.5rem;
+    color: var(--creme);
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    column-gap: 3.25rem;
+}
+
+.logo {
+    height: 4.5rem;
+    margin-bottom: 3rem;
+}
+
+.frase-cabecalho {
+    margin-bottom: 2rem;
+}
+
+.texto-verde {
+    color: var(--verde-medio, #3D6D4A);
+}
+
+.foto-banner {
+    width: 35rem;
+}
+
+@media only screen and (max-width: 1300px) {
+    .banner {
+        padding: 4rem 3.75rem;
+        flex-direction: column;
+        align-items: center;
+        gap: 1rem;
+    }
+
+    .logo {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+    }
+}
+
+@media only screen and (max-width: 767px) {
+    .banner {
+        padding: 4rem 1.5rem;
+    }
+
+    .foto-banner {
+        width: min(100%, 21.25rem);
+    }
+}
+</style>
